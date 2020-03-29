0dae3588dce066f91f8ef4b45321a90cfaf8c4b9
Jakob <jakob.lindblad@servicewell.se>
2020-03-29 23:01:10 +0200

    basic cleanup

--- public/scripts.js
+++ public/scripts.js
@@ -1,33 +1,12 @@
+const civElement = document.getElementById('displayCivilizations');
 
-
-function getData(){
-    getOverallData();
-}
-var input = document.getElementById('getData');
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("myBtn").click();
-  }
-});
-
-function getOverallData(){
+function getCivilizations(){
   fetch('/aoe')
   .then((response) => {
-    return response.json();
+      return response.json();
   })
-  .then((data) => {
-
-    //Hämta ut 'root' och sedan försöka skriva till det
-    //document finns metoder för hur man hittar element
-    var ageElement = document.getElementById('age');
-    var civElement = document.getElementById('civ');
-    var leaderBoardElement = document.getElementById('leaderBoard');
-
-
-    ageElement.innerHTML = JSON.stringify(data.age);
+  .then((data) => {  
     civElement.innerHTML = JSON.stringify(data.civ);
-    leaderBoardElement.innerHTML = JSON.stringify(data.leaderboard);
-  });
+});
 }
 
